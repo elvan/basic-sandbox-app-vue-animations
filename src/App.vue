1c0aea4ca79b874806637b51c134173d05ea0caa
Elvan Hidayat <elvanhd@gmail.com>
2024-03-06 23:46:50 +0700

    Integrate paragraph toggle with transition effect
    - Add a paragraph with conditional rendering controlled by paraIsVisible data property
    - Implement toggleParagraph method to change visibility state
    - Wrap paragraph in <transition> tag with "para" as transition name
    - Define CSS for enter and leave transitions using slide-scale keyframes animation
    - Enhance UI interaction with a button to toggle paragraph visibility

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
     <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
   </div>
+  <div class="container">
+    <transition name="para">
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -18,11 +24,15 @@ export default {
     return {
       animatedBlock: false,
       dialogIsVisible: false,
+      paraIsVisible: false,
     };
   },
   methods: {
     animateBlock() {
       this.animatedBlock = true;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
     },
     showDialog() {
       this.dialogIsVisible = true;
@@ -79,7 +89,36 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-@keyframes slide-fade {
+.para-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+
+.para-enter-active {
+  animation: slide-scale 0.3s ease-out;
+}
+
+.para-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.para-leave-from {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.para-leave-active {
+  /* transition: all 0.3s ease-in; */
+  animation: slide-scale 0.3s ease-out;
+}
+
+.para-leave-to {
+  /* opacity: 0;
+  transform: translateY(30px); */
+}
+
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
