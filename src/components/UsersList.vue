91f633fd1dc41c4ef4f5c49e6b509e248001dd20
Elvan Hidayat <elvanhd@gmail.com>
2024-03-07 00:54:14 +0700

    Introduce UsersList component with add/remove functionality
    - Create UsersList component for displaying a list of users
    - Implement addUser method to add users from an input field
    - Utilize removeUser method to delete a user on click
    - Bind users array to iterate over and display each user in a list
    - Style the UsersList component for better visual presentation
    - Integrate UsersList into App.vue as a child component

---
+++ src/components/UsersList.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+    <li v-for="user in users" :key="user" @click="removeUser(user)">
+      {{ user }}
+    </li>
+  </ul>
+  <div>
+    <input type="text" ref="userNameInput" />
+    <button @click="addUser">Add User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: ['Max', 'Manu', 'Julie', 'Angela', 'Michael'],
+    };
+  },
+  methods: {
+    addUser() {
+      const enteredUserName = this.$refs.userNameInput.value;
+      this.users.unshift(enteredUserName);
+    },
+    removeUser(user) {
+      this.users = this.users.filter((usr) => usr !== user);
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  margin: 1rem 0;
+  padding: 0;
+  list-style: none;
+}
+li {
+  padding: 1rem;
+  border: 1px solid #ccc;
+  text-align: center;
+}
+</style>
